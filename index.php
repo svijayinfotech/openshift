eaaae3f48e4d1e058d8d30ce785dbcc7b9d2d535
root <root@vm.example.edu>
2019-08-12 00:08:20 +0530

    august update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1> This is Openshift Console Based PHP Page 2019 </h1>
+<h1> This is Openshift Console Based PHP Page 2019 - August Month </h1>
 <?php
 echo "This is my openshift world";
 ?>
