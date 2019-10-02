150fe1045a9fd53efb08c538a7ade0ecb2e1501a
root <root@vm.example.edu>
2019-10-02 10:55:19 +0530

    oc console update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1> This is Git Based PHP Page 2019 - October Month </h1>
+<h1> This is OPENSHIFT CONSOLE Based PHP Page 2019 - October Month </h1>
 <?php
 echo "This is my openshift world";
 ?>
