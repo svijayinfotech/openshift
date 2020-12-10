b6b336e5ae0c597a3255cf2edc0edc06124cd74c
root <root@vm.example.edu>
2020-12-10 19:53:20 +0530

    updated

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1> India vs Australia </h1>
-<h1> HUNTERS CRICKET CLUB </h1>
-<h1> HAPPY KARTHIGAI DEEPAM 2020 </h1>
+<h1> FORD </h1>
+<h1> TATA </h1>
+<h1> NISSAN </h1>
 <?php
 ?>
 </body>
